aedf3dfef45847d585e2ce8e9a8d41c0514e03ae
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-03-21 10:27:24 -0400

    Branch: Adding IMenu interface & MainMenu class.

---
+++ EpisodeRenamer/RenamerConsole/Menus/IMenu.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RenamerConsole.Menus {
+    public interface IMenu {
+        Task DisplayMenu();
+    }
+}
