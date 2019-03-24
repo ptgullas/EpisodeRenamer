309db3574a2b41e3d9fa30e972e7042e7af58e44
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-03-24 00:14:52 -0400

    Trimming spaces from the end of GetFormattedFilename

--- EpisodeRenamer/Renamer.Services/Models/EpisodeForComparingDtoExtensions.cs
+++ EpisodeRenamer/Renamer.Services/Models/EpisodeForComparingDtoExtensions.cs
@@ -5,7 +5,7 @@ using System.Text;
 namespace Renamer.Services.Models {
     public static class EpisodeForComparingDtoExtensions {
         public static string GetFormattedFilename(this EpisodeForComparingDto ep) {
-            return ($"{ep.SeriesName} - {ep.GetSeasonEpisodeInNumberFormat()} - {ep.EpisodeTitle}");
+            return ($"{ep.SeriesName} - {ep.GetSeasonEpisodeInNumberFormat()} - {ep.EpisodeTitle}").TrimEnd(' ');
         }
         public static string GetSeasonEpisodeInNumberFormat(this EpisodeForComparingDto ep) {
             return $"{ep.SeasonNumber}.{ep.EpisodeNumberInSeason.ToString("D2")}";
