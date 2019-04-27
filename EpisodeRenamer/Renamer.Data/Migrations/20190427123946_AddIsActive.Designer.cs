b938de5a175d86add8138b6c79785d076f7959c0
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-04-27 08:44:09 -0400

    Added IsActive column to Shows table

--- EpisodeRenamer/Renamer.Data/Migrations/20190427123946_AddIsActive.Designer.cs
+++ EpisodeRenamer/Renamer.Data/Migrations/20190427123946_AddIsActive.Designer.cs
@@ -2,15 +2,17 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using Renamer.Data.Entities;
 
 namespace Renamer.Data.Migrations
 {
     [DbContext(typeof(EpisodeContext))]
-    partial class EpisodeContextModelSnapshot : ModelSnapshot
+    [Migration("20190427123946_AddIsActive")]
+    partial class AddIsActive
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ EpisodeRenamer/Renamer.Data/Migrations/20190427123946_AddIsActive.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Renamer.Data.Migrations
+{
+    public partial class AddIsActive : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<bool>(
+                name: "IsActive",
+                table: "Shows",
+                nullable: false,
+                defaultValue: true);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "IsActive",
+                table: "Shows");
+        }
+    }
+}
