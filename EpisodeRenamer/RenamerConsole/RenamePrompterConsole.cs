5adc826d3eac150c53202f667d779d02f76adcf2
ptgullas <4305689+ptgullas@users.noreply.github.com>
2019-10-12 21:47:22 -0400

    Changed Rename color to Monokai magenta

--- EpisodeRenamer/RenamerConsole/RenamePrompterConsole.cs
+++ EpisodeRenamer/RenamerConsole/RenamePrompterConsole.cs
@@ -8,7 +8,7 @@ namespace RenamerConsole {
     public class RenamePrompterConsole : IRenamePrompter {
         public bool PromptForRename(string localFile, string targetName) {
             Console.Write($"Rename ");
-            MenuHelpers.WriteColorVT24Bit($"{localFile} ", "#41bbd9");
+            MenuHelpers.WriteColorVT24Bit($"{localFile} ", "#F92672");
             Console.Write("to ");
             MenuHelpers.WriteColorVT24Bit($"{targetName}", "#FFFF00");
             Console.WriteLine("?");
